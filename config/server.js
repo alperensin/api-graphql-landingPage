2906a13c7410b342653956fd180fa76988f5862c
alperensin <andreluizperensin@hotmail.com>
2020-12-09 23:17:26 -0300

    Initial Commit | Setup Project | Create Components LandingPage | Add custom text rich editor to admin panel | Transform api rest to graphql.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b38d49d680161446640ba47394300bff'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '15f76c5a-d0c4-41b5-a302-310a5e6873d8'
+};
