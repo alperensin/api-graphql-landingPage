f06c9fbbe334e857000c51f2ede49cdb568b9859
alperensin <andreluizperensin@hotmail.com>
2020-12-13 17:59:12 -0300

    Config static provider aws s3

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'aws-s3',
     providerOptions: {
-      accessKeyId: env('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: env('AWS_ACCESS_SECRET'),
-      region: env('AWS-REGION'),
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      region: env("AWS_REGION"),
       params: {
-        Bucket: env('AWS-BUCKET'),
+        Bucket: env("AWS_BUCKET"),
       },
     },
   },
