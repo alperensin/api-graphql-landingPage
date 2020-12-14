d55fe9cf496bdc3736b61380a09b18f26dac66cd
alperensin <andreluizperensin@hotmail.com>
2020-12-13 22:32:44 -0300

    Add jwt token in .env file and change server.js config

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b38d49d680161446640ba47394300bff'),
+      secret: env("ADMIN_JWT_SECRET"),
     },
   },
 });
